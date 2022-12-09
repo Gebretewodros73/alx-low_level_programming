b491e7e54d623c7f9330522c5f91a3c3aea75269
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-08 20:14:07 -0800

    working with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,6 @@
  */
 int main(void)
 {
-	char puzzel[30] = "\"programming is like building a multilingual puzzle\n";
-
-	puts(puzzel);
+	puts("\"programming is like building a multilingual puzzle\n");
 	return (0);
 }
