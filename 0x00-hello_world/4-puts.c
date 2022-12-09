ca23192578de69ba6ea3a18d299cf2062a74c7cf
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-08 18:39:30 -0800

    first program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include<stdio.h>
 /**
  * main - this prints programming is like building a multilingual puzzle
@@ -6,6 +5,6 @@
  */
 int main(void)
 {
-	puts("programming is like building a multilingual puzzle\n");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
