d90bd75d1d7128722e6d4a77780163a364345606
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-08 19:19:35 -0800

    using print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
  * main - This prints with
- * return: This returns 0
+ * Return: This returns 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");/*my mistake bet*/
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n"); /*my*/
+	return (0); /*beti gn*/
 }
