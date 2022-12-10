78cf2d1fc24c81245f5d40af1228866dc6590ca6
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-09 18:12:40 -0800

    lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - print postive negative and zero
+ * Return: returns zero
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
