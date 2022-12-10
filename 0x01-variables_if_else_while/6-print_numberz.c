1ca5b20ab2d2890b61a0dd92d7be2a9ace5e87a3
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-09 19:07:59 -0800

    using putchar base ten

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - print starting from zero
+ * Return: returns zero
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar((n % 10) + '0');
+		putchar('\n');
+	}
+	return (0);
+}
