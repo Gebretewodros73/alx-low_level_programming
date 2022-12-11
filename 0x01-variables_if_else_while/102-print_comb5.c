a13dd484419fcae6e05cb1451be7dda6d4375e47
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-11 05:37:01 -0800

    combination 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,47 +5,25 @@
  */
 int main(void)
 {
-	int n, m, o, p, i;
+	int n;
+	int m;
 
-	for (n = 0; n <= 9; n++)
+	for (n =0; n < 99; n++)
 	{
-		if (n != 9)
+		for (m = n + 1; m < 100; m++)
 		{
-			i = 9;
-		}
-		else
-		{
-			i = 8;
-		}
-		for (m = 0; m <= i; m++)
-		{
-			if (m != 9)
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+			putchar(' ');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+			if (n != 98)
 			{
-				p = m + 1;
-			}
-			else
-			{
-				p = 0;
-			}
-			for (o = n; o <= 9; o++)
-			{
-				for (; p <= 9; p++)
-				{
-						putchar((n % 10) + '0');
-						putchar((m % 10) + '0');
-						putchar(' ');
-						putchar((o % 10) + '0');
-						putchar((p % 10) + '0');
-						putchar(',');
-						putchar(' ');
-				}
-			p = 0;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-	putchar('\b');
-	putchar('\b');
-	putchar(' ');
-	putchar('\n');
+		putchar('\n');
 	return (0);
 }
