d36df53cef94fad9be42d1e457539a622915180e
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-13 16:21:46 -0800

    _putchar is tidies

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char t);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int t);
+int _isalpha(int t);
+int print_sign(int g);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_table(void);
+int add(int, int);
+void print_to_98(int g);
+void print_times_table(int g);
+
+#endif
