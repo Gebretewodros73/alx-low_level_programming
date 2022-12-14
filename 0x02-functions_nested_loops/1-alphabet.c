109fd5f76e7cb9393734a7d658c7e5f7c141f9d1
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-13 17:05:05 -0800

    print alphabet inorder

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - alphabet in order
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char t;
+
+	for (t = 'a'; t <= 'z'; t++)
+	{
+		_putchar(t);
+	}
+	_putchar('\n');
+}
