77d33c0fa14cac8660a109208af38e00741311a5
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-13 17:24:02 -0800

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - alphabet in order ten times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char t, u;
+
+	for (u = 'a'; u <= 'j'; u++)
+	{
+		for (t = 'a'; t <= 'z'; t++)
+		{
+			_putchar(t);
+		}
+	}
+	_putchar('\n');
+}
