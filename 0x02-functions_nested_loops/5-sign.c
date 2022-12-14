240eff9e2491f614462392597b76ecd84be2309b
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-14 13:42:02 -0800

    detect sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * printt_sign - print sign of a number
+ * print_sign - print sign of a number
  * Return: 1, 0 and -1.
  */
 int print_sign(int g)
@@ -8,14 +8,17 @@ int print_sign(int g)
 
 	if (g > 0)
 	{
+		_putchar('+');
 		return (1);
 	}
 	else if (g < 0)
 	{
+		_putchar('-');
 		return (-1);
 	}
 	else 
 	{
+		_putchar('0');
 		return (0);
 	}
 }
