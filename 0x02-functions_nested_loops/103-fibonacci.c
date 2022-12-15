94ae3e2264e39ec84387b87e48a61396448c03cb
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-15 08:21:01 -0800

    fiboniouns of 400000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,24 +5,17 @@
  */
 int main(void)
 {
-	long int i, fb[32];
+	long int fbs, i, fb[32];
 
 	fb[1] = 1;
 	fb[2] = 2;
+	fbs = 3;
 
-	printf("%ld, %ld, ", fb[1], fb[2]);
-
-	for (i = 3; i <= 32; i++)
+	for (i = 3; i <= 30; i++)
 	{
 		fb[i] = fb[i - 1] + fb[i - 2];
-		if (i != 32)
-		{
-			printf("%ld, ", fb[i]);
-		}
-		else
-		{
-			printf("%ld\n", fb[i]);
-		}
+		fbs = fbs + fb[i];
 	}
+	printf("%ld\n", fbs);
 	return (0);
 }
