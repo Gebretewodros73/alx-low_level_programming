94494619f44aaede65859e597a107c2c3e384ed4
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-15 15:53:27 -0800

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - alphabet in order ten times
+ * @n:number of line
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	char u;
+
+	for (u = 1; u <= n; u++)
+	{
+		_putchar('_');
+	}
+		_putchar('\n');
+}
