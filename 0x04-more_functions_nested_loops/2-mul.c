ded89708f6e5c88e125f3af81c02228233778155
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-15 13:49:21 -0800

    multiplie intiger

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies two intiger
+ * @a: the first intiger
+ * @b: the second intiger
+ * Return: Anything
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
