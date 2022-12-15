2409451889bc2db110273faa8ad676cdae4444c0
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-15 12:42:09 -0800

    largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,23 +5,30 @@
  */
 int main(void)
 {
-	int i, j, t;
+	long int i, j, t;
 
-	/*printf("input your number");
-	printf("\n");
-	scanf("%d", &i);
-	printf("\n");*/
-	for (j = 2; j <= 1231952; j++)
+	printf("input your number\n");
+	scanf("%ld", &i);
+	for (j = 2; j <= i; j++)
 	{
-		t = 1231952 % j;
-
-		if (t = 0)
+		t = i % j;
+		if (t == 0)
 		{
-			printf("The prime factor of the number are : %d",j);
-			printf(",");
-			j = j - 1;
-			i = i / j;
+			if (i != j){
+				printf("%ld,", j);
+				i = i / j;
+				j = j - 1;
+			}
+			else
+			{
+				printf("%ld", j);
+			}
 		}
+		else{
+			continue ;
+		}
+
 	}
+	printf("\n");
 	return (0);
 }
