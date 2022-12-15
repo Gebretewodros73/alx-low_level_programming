17915f5145332e7517fd072be9646556e6a5a067
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-15 05:45:02 -0800

    sumasion of 3 and 5 multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * main - display multiple of 3 and 5
+ * Return: return zero
+ */
+int main(void)
+{
+	int i, j, msthree, msfive, ms, mthree[342], mfive[205];
+
+	msthree = 0;
+	msfive = 0;
+	ms = 0;
+
+	for (i = 1; i <= 341; i++)
+	{
+		mthree[i] = i * 3;
+		msthree = msthree + mthree[i];
+	}
+	for (j = 1; j <= 204; j++)
+	{
+		mfive[j] = j * 5;
+		if (mfive[j] % 3 == 0)
+		{
+			mfive[j] = 0;
+		}
+		msfive = msfive + mfive[j];
+	}
+	ms = msthree + msfive;
+	printf("%d'\n'", ms);
+	return (0);
+}
