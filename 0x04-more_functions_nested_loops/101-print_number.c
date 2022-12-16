f2e9a3a1e15524ef766facf94fc4dafd225ba848
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-16 07:21:32 -0800

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,33 +9,30 @@ void print_number(int n)
 	int s = n, m = n / 10, o = n / 100, p = n % 1000, q = p % 100;
 
 	if (s < 0)
+	{
 		n = n * -1;
-	if (n > 999) {
-		if (s < 0)
-			_putchar('-');
+		_putchar('-');
+	}
+	if (n > 999)
+	{
 		_putchar(o / 10 + '0');
 		_putchar(o % 10 + '0');
 		_putchar(m % 100 + '0');
 		_putchar(q % 10 + '0');
 	}
-	else if (n > 99) {
-		if (s < 0)
-			_putchar('-');
+	else if (n > 99)
+	{
 		_putchar(m / 10 + '0');
 		_putchar(m % 10 + '0');
 		_putchar(n % 100 + '0');
 	}
 	else if (n > 9)
 	{
-		if (s < 0)
-			_putchar('-');
 		_putchar(n / 10 + '0');
 		_putchar(n % 10 + '0');
 	}
 	else
 	{
-		if (s < 0)
-			_putchar('-');
 		_putchar('0' + n);
 	}
 	_putchar('\n');
