6f3a345c30f3f37e1938129957566f5bbbb4202b
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-15 18:24:38 -0800

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_diagonal - alphabet in order ten times
+ * @n:number of line
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	char u;
+
+	for (u = 1; u <= n; u++)
+	{
+		_putchar(92);
+	}
+		_putchar('\n');
+}
