16050eea75e7e266a6fbd13b4a8cfdbb985618d9
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-15 18:54:12 -0800

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_diagonal - alphabet in order ten times
+ * @n:number of line
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (j = 1; j <= size; j++)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+		_putchar('\n');
+}
