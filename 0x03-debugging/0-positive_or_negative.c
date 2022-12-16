cc6f0e901cf464e0d7eeb6fffce1c5a367283dc6
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-15 17:43:29 -0800

    many main

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -9,15 +9,15 @@ void positive_or_negative(int i)
 
 	if (i > 0)
 	{
-		printf("%d is positive\n", i);
+		_printf("%d is positive\n", i);
 	}
 	else if (i == 0)
 	{
-		printf("%d is zero\n", i);
+		_printf("%d is zero\n", i);
 	}
 	else
 	{
-		printf("%d is negative\n", i);
+		_printf("%d is negative\n", i);
 	}
 
 	return (0);
---
+++ 0x03-debugging/_printf.c
@@ -0,0 +1,10 @@
+#include<unistd.h>
+/**
+ * _putchar - t to a standard out put
+ * Return: returns 1
+ */
+
+int _printf(int n)
+{
+	return (printf("%d"));
+}
