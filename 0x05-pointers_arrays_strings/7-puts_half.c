f54cae5da2f69943791ffb8d1cbaf4253910640a
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-19 05:22:28 -0800

    print the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * puts_half - prints the othe half
+ * @str: the character to be ordered
+ * Return: Anything
+ */
+void puts_half(char *str)
+{
+	int i = 0, j;
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+	j = i;
+	if (i % 2 == 0)
+		i = i / 2;
+	else
+		i = (i - 1) / 2;
+	while (i <= j)
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
