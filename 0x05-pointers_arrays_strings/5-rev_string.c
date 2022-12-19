84e15ca7e1047aaac8847c37ee76e26e334b419b
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-19 03:13:52 -0800

    revers string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *rev_string - print revers
+ *@s: string to count
+ *Return: the length of string
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+	char r[100];
+
+	while (*(s + i) != 0)
+	{
+		r[i] = *(s + i);
+		i++;
+	}
+	while (i - 1 >= 0)
+	{
+		*(s + i - 1) = r[j];
+		j++;
+		i--;
+	}
+}
