7793b13d407da0cac334dacf8f47ca825a16ab40
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-22 14:50:11 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_strncpy - length of string
+ *@src: string to count
+ *@dest: string to return
+ *@n: the limit of scr
+ *Return: the length of string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0'){
+		*(dest + i) = src[i];
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
