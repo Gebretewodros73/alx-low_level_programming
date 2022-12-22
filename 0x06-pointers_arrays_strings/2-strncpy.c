d419aa20fce49fda6ba335ae6bfb86a267642c29
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-22 14:56:36 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,10 +10,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n && src[i] != '\0'){
+	while (i < n && src[i] != '\0')
+	{
 		*(dest + i) = src[i];
 		i++;
 	}
-	*(dest + i) = '\0';
+	for (; i < n; i++)
+		*(dest + i) = '\0';
 	return (dest);
 }
