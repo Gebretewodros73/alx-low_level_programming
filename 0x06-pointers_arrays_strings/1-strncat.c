dd4b95e5dda801c36c759cebcf9c8c2abcc324ce
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-22 04:59:23 -0800

    concatnate with n value

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strcat - length of string
+ *@src: string to count
+ *@dest: string to return
+ *@n: the limit of scr
+ *Return: the length of string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, a = 0;
+
+	while (*(dest + i) != 0)
+	{
+		i++;
+	}
+	while (a < n && *(src + a) != 0)
+	{
+		*(dest + i) = *(src + a);
+		i++;
+		a++;
+	}
+	return (dest);
+}
