d8b25c44908f9c7f8ae40a15d84d5d0a957db542
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-22 05:23:04 -0800

    concatnate with n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,17 +8,18 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 1, a = 0;
+	int i = 0, a = 0;
 
 	while (*(dest + i) != 0)
 	{
 		i++;
 	}
-	while (a < n && *(src + a) != 0)
+	while (a < n && *(src + a) != '\0')
 	{
 		*(dest + i) = *(src + a);
 		i++;
 		a++;
 	}
+	*(dest + i) = '\0';
 	return (dest);
 }
