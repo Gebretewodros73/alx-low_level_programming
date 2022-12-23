a1a022355f868c1fea3d7db0736860a51679ecfe
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-22 16:00:18 -0800

    reverse number

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ *reverse_array - revers array
+ *@a: array to revers
+ *@n; max of array
+ *Return: returns Anything
+ **/
+void reverse_array(int *a, int n)
+{
+	int i = n - 1;
+
+	while (i >= 0)
+	{
+		if (i != n - 1)
+			printf(", ");
+		printf("%d", a[i]);
+		i--;
+	}
+}
