436078c2920cc47c30824ede9b116ecf3ed97d64
gebretewodros73 <gebretewodros73@gmail.com>
2022-12-22 17:13:05 -0800

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ *string_toupper - revers array
+ *Return: returns Anything
+ **/
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
