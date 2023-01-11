9e616b89fac5a67552dc6fbab62c659e7f3e3bfe
gebretewodros73 <gebretewodros73@gmail.com>
2023-01-10 17:43:32 -0800

    allocate grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include<stdlib.h>
 /**
- * allloc_grid - allocates a grid, make space and free space
+ * alloc_grid - allocates a grid, make space and free space
  * @width: takes in width of grid
  * @height: height of grid
  * Return: grid with freed spaces
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
-			for (i = 1 - 1; i >= 0; i++)
+			for (i = 1 - 1; i >= 0; i--)
 			{
 				free(grid[i]);
 			}
