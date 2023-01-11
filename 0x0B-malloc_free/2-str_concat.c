be3ff61742d9cca5f789d2abc8d674e369739d87
gebretewodros73 <gebretewodros73@gmail.com>
2023-01-10 17:18:39 -0800

    str contact

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include<string.h>
+#include<stdlib.h>
+/**
+ * str_concat - back a pointer or array
+ * @s1: Array one
+ * @s2: Array two
+ * Return: Always an array dinamic
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *d;
+	unsigned int i, j, size;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	size = (strlen(s1) +strlen(s2) + 1);
+	d = (char *) malloc(size * sizeof(char));
+
+	if (d == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		*(d + i) = *(s1 + i);
+	for(j = 0; *(s2 + j) != '\0'; j++)
+	{
+		*(d + i) = *(s2 + j);
+		i++;
+	}
+	return (d);
+}
