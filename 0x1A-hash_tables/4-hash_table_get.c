7ba65c725c832d6acc30688f8a84d10c9e1d1736
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-08 05:08:49 +0300

    retrieves a value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - value retrieves
+ * @ht: hash table pointer
+ * @key: searched ke
+ *
+ * Return: value to the key coresponding
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL || strlen(key) == 0)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+
+		node = node->next;
+	}
+
+	return (NULL);
+}
+
