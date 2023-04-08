efc8f2d0ccc99fe8c6af90ea74bf448f8b1524a2
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-08 03:05:35 +0300

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,20 +10,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hat;
 
-	if(size == 0)
-		return(NULL);
+	if (size == 0)
+		return (NULL);
 
 	hat = malloc(sizeof(hash_table_t));
-	if(hat == NULL)
-		return(NULL);
+	if (hat == NULL)
+		return (NULL);
 
 	hat->size = size;
 	hat->array = calloc(size, sizeof(hash_node_t *));
-	if(hat->array == NULL)
+	if (hat->array == NULL)
 	{
 		free(hat);
-		return(NULL);
+		return (NULL);
 	}
 
-	return(hat);
+	return (hat);
 }
