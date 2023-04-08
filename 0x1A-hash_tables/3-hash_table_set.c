ded247f22fefc0b0dfe409116734eee8241f69ad
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-08 04:45:03 +0300

    adds an element

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - add to hash table
+ * @ht: points to hash table
+ * @key: key to add
+ * @value: coresponding value of key
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *temp_node;
+
+	if(!ht || !key || !*key || !value)
+		return(0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	temp_node = ht->array[index];
+	while (temp_node)
+	{
+		if (strcmp(temp_node->key, key) == 0)
+		{
+			free(temp_node->value);
+			temp_node->value = strdup(value);
+			return (1);
+		}
+		temp_node = temp_node->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return(1);
+}
