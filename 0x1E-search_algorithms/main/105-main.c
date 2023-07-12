597f0e7a84431bd04243439f3135621bb66ee8e3
gebretewodros73 <gebretewodros73@gamil.com>
2023-07-12 21:29:19 +0300

    implementing the jump search algorithm in a singly linked list.

--- 0x1E-search_algorithms/main/105-main.c
+++ 0x1E-search_algorithms/main/105-main.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "search_algos.h"
+#include "../search_algos.h"
 
 listint_t *create_list(int *array, size_t size);
 void print_list(const listint_t *list);
